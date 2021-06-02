a584a0822ddce7b76749384d4a52d2a4b60c77c4
萩原涼【マーケティング】(ryo hagiwara) <ryo_hagiwara@mates-system.com>
2021-06-02 10:35:47 +0900

    update: irf_msci.R

--- R/irf_msci.R
+++ R/irf_msci.R
@@ -4,8 +4,10 @@ msci <- read.csv("Datasets/msci.csv",row.names=1)
 
 msci.var<-VAR(msci,p=3)
 
-# 14期先までインパルス応答関数を計算
-msci.irf<-irf(msci.var,n.ahead=14,ci=0.95)
+# n.ahead=14: 14期先までインパルス応答関数を計算
+# ci=0.95: 信頼区間95%
+# orth=TRUEで直交化インパルス応答、FALSEで非直交化インパルス応答
+msci.irf<-irf(msci.var,n.ahead=14,ci=0.95,orth = TRUE)
 
 plot(msci.irf)
 
